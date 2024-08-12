38d7fb5580fe735205b7220d3d47f98f247cb066
Leo-Dev <leonardocaimmi1@gmail.com>
2024-08-12 14:12:56 -0300

    practica let,const

---
+++ PracticaClase1/javaScript/variables.js
@@ -0,0 +1,13 @@
+//Variables: let, const
+//1. Declarar una variable `nombre` y asignarle tu nombre. Imprimir en la consola.
+let nombre = "Leo";
+const nombre2 = "Leo";
+console.log(nombre);
+console.log(nombre2);
+//2. Declarar dos variables numéricas `num1` y `num2`, y calcular su suma. Imprimir el resultado.
+let num1 = 14;
+let num2 = 16;
+console.log("suma ",num1+num2);
+//3. Declarar una constante `PI` con el valor 3.14159. Imprimir en la consola.
+const PI = 3.14159;
+console.log(PI);
