a81769b785d1d26334b604230ecb11b6cde4dade
Leo-Dev <leonardocaimmi1@gmail.com>
2024-08-19 11:29:13 -0300

    arrays

--- PracticaClase1/Objetos Arreglos Basicos/js/variables.js
+++ PracticaClase1/Objetos Arreglos Basicos/js/variables.js
@@ -28,3 +28,33 @@ console.log(persona);
 //Muestra el objeto actualizado en la consola.
 persona.email = "leo@hotmail.com";
 console.log(persona);
+//6. Crear un Array de Números
+//• Crea un array llamado “numeros” que contenga los números del 1 al 5.
+//Muestra el array en la consola.
+const numeros =[1,2,3,4,5];
+console.log(numeros);
+//7. Acceder a un Elemento de un Array
+//• Accede y muestra en la consola el tercer elemento del array “numeros”.
+console.log(numeros[2]);
+//8. Agregar un Elemento a un Array
+//• Agrega el número 6 al final del array “numeros” y muestra el array
+//actualizado en la consola.
+numeros.push(6);
+console.log(numeros);
+//9. Eliminar el Último Elemento de un Array
+//• Elimina el último elemento del array “numeros” y muestra el array
+//actualizado en la consola.
+numeros.pop();
+console.log(numeros);
+//10. Recorrer un Array con un Bucle
+//• Usa un bucle “for” para recorrer el array “numeros” y muestra cada
+//elemento en la consola.
+for(let i = 0;i<numeros.length;i++)
+{
+    console.log(numeros[i]);
+}
+for(let i of numeros)
+    {
+        console.log(i);
+    }
+    numeros.forEach((valor)=>{console.log(valor)});
