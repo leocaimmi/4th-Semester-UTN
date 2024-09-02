21d2ac99c945696346cc4400bcde3afc422b1b3e
Leo-Dev <leonardocaimmi1@gmail.com>
2024-09-02 18:57:33 -0300

    p 1 y 2

---
+++ Callbacks/tp1/js/main.js
@@ -0,0 +1,26 @@
+/*Ejercicio 1: Función Callback Básica
+Escribe una función llamada `mostrarMensaje` que acepte un mensaje como argumento y una
+función de callback. La función `mostrarMensaje` debe mostrar el mensaje y luego llamar a la
+función de callback.*/
+const callback1 = function(msj)
+{
+console.log(msj);
+}
+function mostrarMensaje(msj,callback)
+{
+callback1(msj);
+}
+mostrarMensaje("Hola como estas?",callback1);
+/*Ejercicio 2: Callback con Retorno de Datos
+Crea una función llamada `sumarAsync` que acepte dos números y una función de callback.
+Esta función debe sumar los números y luego llamar a la función de callback con el resultado.*/ 
+
+function sumarAsync(a,b,callback2)
+{
+const resultado = a+b;
+callback2(resultado);
+}
+
+sumarAsync(10,90,(suma)=>console.log(`la suma es ${suma}`));
+
+
