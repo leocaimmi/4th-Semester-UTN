fad7f15cadf20310a9fe9120c461d23bfb31a8a1
Leo-Dev <leonardocaimmi1@gmail.com>
2024-10-07 13:54:57 -0300

    checkbox entrelazado

--- Angular/tp-dataBinding/src/app/components/productos/productos.component.ts
+++ Angular/tp-dataBinding/src/app/components/productos/productos.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
